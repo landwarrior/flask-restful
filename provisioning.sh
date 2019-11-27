221b2815f14bedc40b128b1decf499279eeeeb36
landwarrior <whippoorwill2zosterops@outlook.jp>
2019-11-28 00:54:20 +0900

    modify provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -9,8 +9,16 @@ sudo yum update -y
 echo ""
 
 # とりあえず開発ツール関係を入れておく
-echo "  - yum groupinstall \"Development Tools\""
-sudo yum groupinstall -y "Development Tools"
+if ! rpm -qa | grep gcc | grep -v libgcc ; then
+    echo "  - yum groupinstall \"Development Tools\""
+    sudo yum groupinstall -y "Development Tools"
+fi
+
+# 保管してくれるやつ
+if ! rpm -qa | grep bash-completion ; then
+    echo "  - yum install bash-completion"
+    sudo yum install -y bash-completion
+fi
 
 # chef インストール
 if [ $(rpm -qa | grep chef) ]; then
@@ -24,12 +32,12 @@ echo ""
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-if [ ! "$(rpm -qa | grep python3)" ]; then
+if  ! rpm -qa | grep python3 ; then
     # Python3系のリポジトリを追加する
     echo "  - add ius repo"
     yum install -y https://centos7.iuscommunity.org/ius-release.rpm
     echo "  - install python36"
-    yum install -y python36u python36u-libs python36u-devel python36u-pip
+    yum install -y python3 python3-libs python3-devel python3-pip
     echo "  - pip install"
     pip3.6 install -r "$SCRIPT_DIR/docker/flask/requirements.txt"
 else
@@ -40,7 +48,7 @@ echo ""
 
 # chef 実行(ライセンスへの同意を求められたら yes を入力してください)
 # それぞれのファイルはフルパスじゃないとちゃんと動かないようだ・・・
-sudo chef-solo -c /vagrant_data/chef-repo/solo.rb -j /vagrant_data/chef-repo/nodes/flask-restful.json
+sudo chef-client -z -c /vagrant_data/chef-repo/solo.rb -j /vagrant_data/chef-repo/nodes/flask-restful.json
 
 # ここからは docker 用の準備
 
@@ -54,5 +62,9 @@ echo "  - docker build"
 docker build -t flask /var/app/docker/flask
 docker build -t nginx /var/app/docker/nginx
 
-echo "  - docker swarm init"
-docker swarm init --advertise-addr 192.168.33.10
+if ! docker node ls -q ; then
+    echo "  - docker swarm init"
+    docker swarm init --advertise-addr 192.168.33.10
+else
+    echo "  * skip docker swarm init"
+fi
